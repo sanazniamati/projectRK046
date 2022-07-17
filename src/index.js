013259edcbee71236a644398157327586926385e
sanazniamati <sanaz.niamati1991@gmail.com>
2022-07-17 18:06:22 +0430

    2

--- src/index.js
+++ src/index.js
@@ -5,39 +5,41 @@ import { Stage, Layer, Text, Circle } from "react-konva";
 const App = () => {
   const [text, setText] = useState("");
   let [count, setCount] = useState(1);
-  const handelMouseOver = () => {
+  const handelMouseOver = (e) => {
     setCount((count) => count + 1);
-    setText("MouseOver " + count);
-    console.log(count);
+    setText(" events " + e.type + count);
+    console.log("events: " + count);
   };
-  const handelMouseUp = () => {
+  const handelMouseUp = (e) => {
     setCount((count) => count + 1);
     setText("MouseUp " + count);
-    console.log(count);
+    console.log("events: " + count);
+    console.log(e);
   };
-
-  const handelMouseDown = () => {
+  const handelMouseDown = (e) => {
     setCount((count) => count + 1);
     setText("MouseDown " + count);
-    console.log(count);
+    console.log("events: " + count);
   };
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Text text={text} x={10} y={10} fontSize={24} fill={"black"} />
-        <Circle
-          x={150}
-          y={150}
-          radius={70}
-          fill={"red"}
-          stroke={"black"}
-          strokeWidth={4}
-          onMouseOver={handelMouseOver}
-          onMouseUp={handelMouseUp}
-          onMouseDown={handelMouseDown}
-        />
-      </Layer>
-    </Stage>
+    <>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text={text} x={10} y={10} fontSize={24} fill={"black"} />
+          <Circle
+            x={150}
+            y={150}
+            radius={70}
+            fill={"red"}
+            stroke={"black"}
+            strokeWidth={4}
+            onMouseOver={handelMouseOver}
+            onMouseUp={handelMouseUp}
+            onMouseDown={handelMouseDown}
+          />
+        </Layer>
+      </Stage>
+    </>
   );
 };
 
